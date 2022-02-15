163cad83dbcd0ebca98d82626981b4aa40a64d91
Adam <44712760+Depth-Hoar@users.noreply.github.com>
2022-02-15 10:53:43 -0700

    switch-page-accounts

--- packages/page-ddelegation/src/index.tsx
+++ packages/page-ddelegation/src/index.tsx
@@ -1,55 +1,65 @@
-// Copyright 2017-2021 @polkadot/app-democracy authors & contributors
+// Copyright 2017-2021 @polkadot/app-accounts authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { useMemo } from 'react';
+import type { AppProps as Props } from '@polkadot/react-components/types';
+
+import React, { useRef } from 'react';
 import { Route, Switch } from 'react-router';
 
 import { HelpOverlay, Tabs } from '@polkadot/react-components';
+import { useAccounts, useIpfs } from '@polkadot/react-hooks';
 
-import useDispatchCounter from './Execute/useCounter';
 import basicMd from './md/basic.md';
-import Execute from './Execute';
-import Overview from './Overview';
+import Accounts from './Accounts';
 import { useTranslation } from './translate';
+import useCounter from './useCounter';
+import Vanity from './Vanity';
 
-export { default as useCounter } from './useCounter';
+export { useCounter };
 
-interface Props {
-  basePath: string;
-}
+const HIDDEN_ACC = ['vanity'];
 
-function DemocracyApp ({ basePath }: Props): React.ReactElement<Props> {
+function AccountsApp ({ basePath, onStatusChange }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
-  const dispatchCount = useDispatchCounter();
+  const { hasAccounts } = useAccounts();
+  const { isIpfs } = useIpfs();
 
-  const items = useMemo(() => [
+  const tabsRef = useRef([
     {
       isRoot: true,
       name: 'overview',
-      text: t<string>('Overview')
+      text: t<string>('My accounts')
     },
     {
-      count: dispatchCount,
-      name: 'dispatch',
-      text: t<string>('Dispatch')
+      name: 'vanity',
+      text: t<string>('Vanity generator')
     }
-  ], [dispatchCount, t]);
+  ]);
 
   return (
-    <main className='democracy--App'>
+    <main className='accounts--App'>
       <HelpOverlay md={basicMd as string} />
       <Tabs
         basePath={basePath}
-        items={items}
+        hidden={(hasAccounts && !isIpfs) ? undefined : HIDDEN_ACC}
+        items={tabsRef.current}
       />
       <Switch>
-        <Route path={`${basePath}/dispatch`}>
-          <Execute />
+        <Route path={`${basePath}/vanity`}>
+          <Vanity
+            basePath={basePath}
+            onStatusChange={onStatusChange}
+          />
         </Route>
-        <Route><Overview /></Route>
+        <Route>
+          <Accounts
+            basePath={basePath}
+            onStatusChange={onStatusChange}
+          />
+        </Route>
       </Switch>
     </main>
   );
 }
 
-export default React.memo(DemocracyApp);
+export default React.memo(AccountsApp);
--- packages/page-ddelegation/src/useCounter.ts
+++ packages/page-ddelegation/src/useCounter.ts
@@ -1,24 +1,10 @@
-// Copyright 2017-2021 @polkadot/app-democracy authors & contributors
+// Copyright 2017-2021 @polkadot/app-accounts authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useEffect, useState } from 'react';
+import { useAccounts } from '@polkadot/react-hooks';
 
-import { useAccounts, useApi, useCall, useIsMountedRef } from '@polkadot/react-hooks';
-
-export default function useCounter (): number {
+export default function useCounter (): string | null {
   const { hasAccounts } = useAccounts();
-  const { api, isApiReady } = useApi();
-  const mountedRef = useIsMountedRef();
-  const proposals = useCall<unknown[]>(isApiReady && hasAccounts && api.derive.democracy?.proposals);
-  const referenda = useCall<unknown[]>(isApiReady && hasAccounts && api.derive.democracy?.referendumsActive);
-  const [counter, setCounter] = useState(0);
 
-  useEffect((): void => {
-    mountedRef.current && setCounter(
-      (proposals?.length || 0) +
-      (referenda?.length || 0)
-    );
-  }, [mountedRef, proposals, referenda]);
-
-  return counter;
+  return hasAccounts ? null : '!';
 }
