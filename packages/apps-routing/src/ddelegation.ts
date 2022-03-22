1b83e73a27eba6172f7fa780e3c4a8cff5977ded
Adam <44712760+Depth-Hoar@users.noreply.github.com>
2022-03-22 15:45:30 -0600

    Change delegation icon

--- packages/apps-routing/src/ddelegation.ts
+++ packages/apps-routing/src/ddelegation.ts
@@ -15,7 +15,7 @@ export default function create (t: TFunction): Route {
       ]
     },
     group: 'governance',
-    icon: 'calendar-check',
+    icon: 'vote-nay',
     name: 'delegation',
     text: t('nav.ddelegation', 'Delegation', { ns: 'apps-routing' }),
     useCounter
